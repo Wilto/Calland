3b9082e5c4765b42ce568e93da7d0da47d4568a1
Mat Marquis <mat@matmarquis.com>
2011-04-05 22:53:34 -0400

    Images now resize to (a maximum of) the viewport width, rather than 25% of the original.

--- images/img.php
+++ images/img.php
@@ -1,63 +1,64 @@
 <?php
-class rwdImage {
+	class rwdImage {
 	
-	function rwdImage($image) {
-		$size = GetImageSize($image);
-		if(!$image || substr($size['mime'], 0, 6) != 'image/') {
-			header('HTTP/1.1 400 Bad Request');
-			exit();
+		function rwdImage($image) {
+			$size = GetImageSize($image);
+			if(!$image || substr($size['mime'], 0, 6) != 'image/') {
+				header('HTTP/1.1 400 Bad Request');
+				exit();
+			}
 		}
-	}
 	
-	function determineType($image) {
-		$path_parts = pathinfo($image);
-		$ext = strtolower($path_parts["extension"]);
+		function determineType($image) {
+			$path_parts = pathinfo($image);
+			$ext = strtolower($path_parts["extension"]);
 	
-		switch ($ext) { 
-			case "gif":
-				$type = "gif";
-				break; 
-			case "png":
-				$type = "png";
-				break; 
-			case "jpeg": 
-			case "jpg":
-				$type = "jpg"; 
-				break; 
+			switch ($ext) { 
+				case "gif":
+					$type = "gif";
+					break; 
+				case "png":
+					$type = "png";
+					break; 
+				case "jpeg": 
+				case "jpg":
+					$type = "jpg"; 
+					break; 
+			}
+			return $type;
 		}
-		return $type;
-	}
 
-	function resize($target, $path, $scale, $type) {
-		$imageTmp = imagecreatefromstring($target);
+		function resize($target, $path, $view, $type) {
+
+			$imageTmp = imagecreatefromstring($target);
 	
-		list($width, $height) = getimagesize($path);
+			list($width, $height) = getimagesize($path);
 	
-		$xscale = $width * $scale;
-		$yscale = $height * $scale;
+			$xscale = $view;
+			$yscale = ($view * ($height / $width));
+
+			$imageResized = imagecreatetruecolor($xscale, $yscale);
 	
-		$imageResized = imagecreatetruecolor($xscale, $yscale);
-	
-		imagecopyresampled($imageResized, $imageTmp, 0, 0, 0, 0, $xscale, $yscale, $width, $height);
+			imagecopyresampled($imageResized, $imageTmp, 0, 0, 0, 0, $xscale, $yscale, $width, $height);
 		
-		switch($type) {
-			case "gif":
-				$imageResized = imagegif($imageResized);
-				break; 
-			case "png":
-				$imageResized = imagepng($imageResized);	
-				break; 
-			case "jpeg": 
-			case "jpg":
-				$imageResized = imagejpeg($imageResized);
-				break; 
-			default:
-				$imageResized = imagepng($imageResized);
-				break;
+			switch($type) {
+				case "gif":
+					$imageResized = imagegif($imageResized);
+					break; 
+				case "png":
+					$imageResized = imagepng($imageResized);	
+					break; 
+				case "jpeg": 
+				case "jpg":
+					$imageResized = imagejpeg($imageResized);
+					break; 
+				default:
+					$imageResized = imagepng($imageResized);
+					break;
+			}
+			return $imageResized;
 		}
-		return $imageResized;
 	}
-}
 	session_start();
 	
 	$path	= $_GET['path'];
@@ -71,8 +72,8 @@ class rwdImage {
 		$_SESSION['rwd-viewport'] = $view = $_COOKIE['rwd-viewport'];
 	}
 	
-	if($view == 'small' || !$view) {
-		$image = $rwd->resize($image, $path, .25, $type);
+	if($view < 480 || !$view) {
+		$image = $rwd->resize($image, $path, $view, $type);
 	}
 
 	header("Content-Type: image/$type");
