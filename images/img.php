fd5b6190bcbd4f093f6099ffec19c9a96e1038f7
Mat Marquis <mat@matmarquis.com>
2011-05-17 12:28:22 -0400

    Updated and classed-up the readme.

--- images/img.php
+++ images/img.php
@@ -76,11 +76,8 @@
 		$image = $rwd->resize($image, $path, $view, $type);
 	}
 
-	header("Cache-Control: private, max-age=10800, pre-check=10800");
-	header("Pragma: private");
-	header("Expires: " . date(DATE_RFC822, strtotime(" 2 day")));
+
 	header("Content-Type: image/$type");
-	
 	print $image;
 	imagedestroy($image);
 ?>
